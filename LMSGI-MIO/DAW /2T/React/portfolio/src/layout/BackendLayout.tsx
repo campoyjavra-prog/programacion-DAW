aef68ea8e06a455b51b51c954ffb99f5010d264a
campoyjavra-prog <campoyjavra@gmail.com>
2026-02-17 12:22:35 +0100

    “node”

---
+++ LMSGI-MIO/DAW /2T/React/portfolio/src/layout/BackendLayout.tsx
@@ -0,0 +1,9 @@
+// sera la parte privada de la aplicacion, realizaar las inserciones de usuarios, clientes, productos, etc
+
+function BackendLayout() {
+    return (
+        <h1>Backend Layout</h1>
+    );
+}
+
+export default BackendLayout;
---
+++ LMSGI-MIO/DAW /2T/React/portfolio/src/layout/MainLayput.tsx
@@ -0,0 +1,13 @@
+// este componente cre el front-end de la aplicacion, es la parte publica
+
+import Header from "../components/main/Header";
+
+function MainLayout() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
+
+export default MainLayout;
